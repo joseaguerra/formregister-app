9dd4587d4331af0c7a7af0c92491ddf259fb952b
Jose Guerra <joseguerra@MacBook-Pro-Chacho.local>
2022-11-14 19:19:14 +0100

    migrate: add migrations

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,5 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+DATABASE_URL="postgresql://postgres:DXRuSgbREKqzu9WbiCQE@containers-us-west-118.railway.app:5442/railway"
