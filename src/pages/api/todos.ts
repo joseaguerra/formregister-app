39bed646829b9cd56580863c2e98890c9c191267
Jose Guerra <joseguerra@MacBook-Pro-Chacho.local>
2022-11-14 20:03:59 +0100

    fix: delete Todo reference

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -1,38 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from ".prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = PrismaClient
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     // get all todos
-    const todos = await prisma.todo.findMany({
+    const todos = await prisma.customer.findMany({
       orderBy: { createdAt: "desc" },
     });
     res.json(todos);
   } else if (req.method === "POST") {
-    // create todo
-    const text = JSON.parse(req.body).text;
-    const todo = await prisma.todo.create({
-      data: { text, completed: false },
-    });
-
-    res.json(todo);
+    //create customer
   } else if (req.method === "PUT") {
-    // update todo
-    const id = req.query.todoId as string;
-    const data = JSON.parse(req.body);
-    const todo = await prisma.todo.update({
-      where: { id },
-      data,
-    });
-
-    res.json(todo);
+    // update customer
   } else if (req.method === "DELETE") {
-    // delete todo
-    const id = req.query.todoId as string;
-    await prisma.todo.delete({ where: { id } });
-
-    res.json({ status: "ok" });
+    // delete customer
   }
 };
